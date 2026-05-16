3a822d7e4f12f1bb5c6befdb4e0f280cda01969f
sykim0181 <soyeon364@naver.com>
2026-05-16 13:50:34 +0900

    feat: Liveblocks의 RoomProvider 대체
    - 웹소켓 연결 중 로딩 스피너 표시

--- src/components/layout/RoomLayout.tsx
+++ src/components/layout/RoomLayout.tsx
@@ -1,4 +1,3 @@
-import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
 import { Outlet, useLoaderData, useParams } from "react-router";
 import { SidebarInset, SidebarProvider } from "../ui/sidebar";
 import RoomSidebar from "../room/sidebar/RoomSidebar";
@@ -8,9 +7,40 @@ import { cn } from "@/lib/utils";
 import RoomAlert from "../room/common/RoomAlert";
 import RoomErrorBoundary from "../room/common/RoomErrorBoundary";
 import User from "../room/common/User";
-import { initialRoomStorage } from "@/constants";
 import FloatingButtonBar from "../room/common/FloatingButtonBar";
 import RoomAccessGuard from "../room/common/RoomAccessGuard";
+import { RoomContextProvider, useRoomContext } from "@/context/RoomContext";
+import { Spinner } from "../ui/spinner";
+
+const RoomLayoutContent = ({ room }: { room: TRoom }) => {
+  const { isConnected } = useRoomContext();
+
+  if (!isConnected) {
+    return (
+      <div className="flex h-dvh items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <SidebarProvider className="h-dvh">
+      <RoomSidebar />
+      <SidebarInset className={cn(["overflow-hidden"])}>
+        <Card className={cn(["h-full", "p-[1rem]"])}>
+          <RoomErrorBoundary>
+            <Outlet context={{ room }} />
+          </RoomErrorBoundary>
+        </Card>
+        <FloatingButtonBar room={room} />
+      </SidebarInset>
+
+      <RoomAlert roomId={room.id} />
+      <User />
+      <RoomAccessGuard room={room} />
+    </SidebarProvider>
+  );
+};
 
 const RoomLayout = () => {
   const params = useParams();
@@ -23,34 +53,9 @@ const RoomLayout = () => {
   const room = useLoaderData() as TRoom;
 
   return (
-    <RoomProvider
-      id={roomId}
-      initialStorage={initialRoomStorage}
-      initialPresence={{
-        name: "unknown",
-      }}
-    >
-      <SidebarProvider className="h-dvh">
-        <RoomSidebar />
-        <SidebarInset className={cn(["overflow-hidden"])}>
-          <Card className={cn(["h-full", "p-[1rem]"])}>
-            <RoomErrorBoundary>
-              <Outlet context={{ room }} />
-            </RoomErrorBoundary>
-          </Card>
-
-          <ClientSideSuspense fallback={null}>
-            <FloatingButtonBar room={room} />
-          </ClientSideSuspense>
-        </SidebarInset>
-      </SidebarProvider>
-
-      <ClientSideSuspense fallback={null}>
-        <RoomAlert roomId={room.id} />
-        <User />
-        <RoomAccessGuard room={room} />
-      </ClientSideSuspense>
-    </RoomProvider>
+    <RoomContextProvider roomId={roomId}>
+      <RoomLayoutContent room={room} />
+    </RoomContextProvider>
   );
 };
 
