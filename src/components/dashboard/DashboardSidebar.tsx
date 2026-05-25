8e31a897bda70e355ab26c708ddd4141d429abb6
sykim0181 <soyeon364@naver.com>
2026-05-25 21:14:17 +0900

    feat: 대시보드 프로필 이름 클릭 시 변경창 나타나는 기능 추가

--- src/components/dashboard/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar.tsx
@@ -4,6 +4,8 @@ import { Link, useLocation } from "react-router";
 import { LayoutDashboardIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { sidebarMenuButtonVariants } from "@/components/ui/sidebar";
+import EditProfileDialog from "@/components/common/EditProfileDialog";
+import { Button } from "@/components/ui/button";
 
 const DashboardSidebar = () => {
   const user = useAppSelector((state) => state.user.user);
@@ -14,7 +16,13 @@ const DashboardSidebar = () => {
     <aside className="hidden md:flex flex-col w-[256px] shrink-0 border-r p-[1rem] gap-16">
       <div className="flex flex-col items-center gap-[0.5rem] pt-[1rem]">
         <UserAvatar userName={user.name} size={64} />
-        <span className="text-sm font-medium">{user.name}</span>
+        <EditProfileDialog
+          trigger={
+            <Button variant="ghost" className="h-auto py-1 px-2 text-base ">
+              {user.name}
+            </Button>
+          }
+        />
       </div>
       <nav>
         <Link
@@ -22,7 +30,7 @@ const DashboardSidebar = () => {
           className={cn(
             sidebarMenuButtonVariants({ size: "default" }),
             isActive &&
-              "bg-sidebar-accent font-medium text-sidebar-accent-foreground"
+              "bg-sidebar-accent font-medium text-sidebar-accent-foreground",
           )}
         >
           <LayoutDashboardIcon />
